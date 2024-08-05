361b4324847bab4679c2a4c8d04f82a7fb094f36
ZeBoulder <kylekirsh@gmail.com>
2024-08-05 10:45:36 +0200

    03 - fontawesome imported

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './components/canvas/canvas.component';
 import { ToolAreaComponent } from './components/tool-area/tool-area.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, CanvasComponent, ToolAreaComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
